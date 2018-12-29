dfc74e25b9da6ec381c29980a9d6241510bcfcb4
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-12-29 15:21:32 +0100

    WIP: Try fixup missing mcpcom and -limf
    We should probably make sure that we properly source the compilervars.sh
    before invoking anything. Then it should work. Hm....

--- pkgs/development/compilers/intel-parallel-studio-xe/default.nix
+++ pkgs/development/compilers/intel-parallel-studio-xe/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, rpm, cpio, autoPatchelfHook, wrapCCWith }:
+{ stdenv, fetchurl, rpm, cpio, autoPatchelfHook, wrapCCWith, makeWrapper}:
 let
   license = /tmp/intel.lic;
   gcc = if stdenv.cc.isGNU then stdenv.cc.cc else stdenv.cc.cc.gcc;
@@ -9,7 +9,7 @@ let
       url = "http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/12998/parallel_studio_xe_${version}_cluster_edition.tgz";
       sha256 = "11wjjaj0mc4w874dchaygz4qar58qvsf0jkvxyp6w1d5rs44pii3";
     };
-    nativeBuildInputs = [ rpm cpio autoPatchelfHook ];
+    nativeBuildInputs = [ rpm cpio autoPatchelfHook makeWrapper ];
     buildInputs = [ stdenv.cc.cc.lib ];
     installPhase = let
       rpms = [ "intel-c-*.rpm" "intel-comp-*" "intel-icc-*.rpm" "intel-openmp-*.rpm"];
@@ -32,6 +32,18 @@ let
     postInstall = ''
       # TODO: The offload_main program needs "libcoi_device.so.0"; not yet in nixpkgs.
       find $out -name offload_main -delete
+
+      # icc/icpc make a call to "gcc -print-search-dirs", so they need gcc in PATH
+      # otherwise we get: "Requires 'install path' setting gathered from 'gcc'"
+      for f in icc icpc xild xiar; do
+        echo "FUNK $f"
+        # TODO source compilervars.sh (even instead of mcpcom stuff)
+        # TODO: SOURCING DOES NOT WORK! :-(
+        wrapProgram $out/bin/$f \
+          --run "source $out/bin/compilervars.sh intel64; echo $INSTALL_DIR; echo OMG" \
+          --set PATH "$(dirname $(echo $out/compilers_and_libraries_*/linux/bin/intel64/mcpcom)):${stdenv.lib.makeBinPath [ stdenv.cc ]}"
+      done
+      # TODO: Use xild
     '';
     passthru = {
       isIntel = true;
