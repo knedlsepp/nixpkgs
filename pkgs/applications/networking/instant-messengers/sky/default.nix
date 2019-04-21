ff529a2447d53f29edded0c8d7441393ce83f2ab
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2019-04-21 19:41:01 +0200

    sky: Fix build

--- pkgs/applications/networking/instant-messengers/sky/default.nix
+++ pkgs/applications/networking/instant-messengers/sky/default.nix
@@ -1,10 +1,6 @@
-{ stdenv, fetchurl, file, lib, libX11, libXScrnSaver
-, libGL, qt5, SDL, libpulseaudio
-, libXrandr, libXext, libXcursor, libXinerama, libXi
-, curl, sqlite, openssl
-, libuuid, openh264, libv4l, libxkbfile, libXv, zlib, libXmu
-, libXtst, libXdamage, pam, patchelfUnstable, libXfixes, libXrender, libjpeg_original
-, ffmpeg
+{ stdenv, fetchurl
+, autoPatchelfHook
+, curl, ffmpeg, libjpeg_original, libuuid, libv4l, openh264, pam, rpmextract, qt5, SDL, xorg
 }:
  let
    # Sky is linked to the libjpeg 8 version and checks for the version number in the code.
@@ -20,58 +16,43 @@ stdenv.mkDerivation rec {
   version_minor = "1";
   version = version_major + "." + version_minor;
   name = "sky-${version}";
-  unpackCmd = "ar x $curSrc; tar -xf data.tar.xz";
   src = fetchurl {
-    url = "https://tel.red/repos/ubuntu/pool/non-free/sky_${version_major + "-" + version_minor}ubuntu+xenial_amd64.deb";
-    sha256 = "0b3j90km3rp5bgaklxw881g0gcy09mqzbhjdfrq4s2np026ql3d9";
+    url = "https://tel.red/repos/opensuse/42.3/x86_64/sky-${version_major}-${version_minor}.osu42.3.x86_64.rpm";
+    sha256 = "0wdh7ixj5y5wldj3i5j4y56c6a91jcf0gnzpz198v9plkg6yl0p7";
   };
-  buildInputs = [ 
-    file
+  nativeBuildInputs = [ autoPatchelfHook rpmextract ];
+  unpackCmd = "rpmextract $curSrc";
+  buildInputs = [
+    curl
+    ffmpeg
+    libjpeg_original_fix
+    libv4l
+    pam
     qt5.qtbase
     SDL
-    ffmpeg
-    sqlite
-    openssl
-    openh264
-    pam
-    curl
-    libX11 libXScrnSaver libGL libpulseaudio libXrandr
-    libXext libXcursor libXinerama libXi libuuid libv4l
-    libxkbfile libXv zlib libXmu libXtst libXdamage
-    libXfixes libXrender
-    libjpeg_original_fix
+    stdenv.cc.cc
+    xorg.libX11
+    xorg.libXdamage
+    xorg.libxkbfile
+    xorg.libXmu
+    xorg.libXScrnSaver
+    xorg.libXtst
+    xorg.libXv
   ];
-  dontBuild = true;
-
   installPhase = ''
-    ls -al ./
-    mkdir -p "$out/bin" "$out/lib" "$out/share"
-    cp -a lib/sky/* $out/bin/
-    cp -aR lib/sky/lib64/* "$out/lib/"
-    cp -a lib/sky/man.sh "$out/bin"
-    chmod +x $out/bin/sky
-    cp -a share/* "$out/share"
-  ''
-  ;
-
-
-  postFixup = ''
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libfreerdp-client.so.2.0.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libfreerdp-server.so.2.0.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libfreerdp-shadow.so.2.0.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libfreerdp.so.2.0.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libopenh264.so.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/librdtk.so.1.1.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libSDL-1.3.so.0.0.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libsipw.so.1.0.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libwinpr.so.1.1.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} $out/lib/libxfreerdp-client.so.2.0.0
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) $out/bin/sky
-    patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib${stdenv.lib.optionalString stdenv.is64bit "64"}:${stdenv.lib.makeLibraryPath buildInputs} --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) $out/bin/sky_sender
-    sed -i "s#/usr/bin/sky#$out/bin/sky#g" $out/share/applications/sky.desktop
-    sed -i "s#/usr/lib/sky#$out/bin/#g" $out/share/applications/sky.desktop
+    mkdir -p $out
+    cp -r * $out
+    mkdir -p $out/bin
+    ln -sf  $out/lib*/sky/sky $out/bin/
+    ln -sf $out/lib*/sky/sky_sender $out/bin/
   '';
-
+  preFixup = ''
+    find $out
+    substituteInPlace $out/share/applications/sky.desktop \
+          --replace "Exec=/usr/bin/sky" "Exec=$out/bin/sky" \
+          --replace "Path=/usr/lib/sky" ""
+  '';
+  dontStrip = true;
   meta = with stdenv.lib; {
     description = "Skype for business";
     longDescription = ''
@@ -80,7 +61,7 @@ stdenv.mkDerivation rec {
     homepage = https://tel.red/;
     license = licenses.unfree;
     maintainers = [ maintainers.Scriptkiddi ];
-    platforms = platforms.unix;
+    platforms = [ "x86_64-linux" ];
   };
 }
 
