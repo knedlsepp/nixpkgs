70bebca015fb604f94b246a67862c2d349fa7165
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2020-07-01 20:03:44 +0200

    WIP: python-mapnik: Fix proj dependency
    Fixes: #91889

--- pkgs/development/python-modules/python-mapnik/default.nix
+++ pkgs/development/python-modules/python-mapnik/default.nix
@@ -5,6 +5,9 @@
 , pkgs
 , pillow
 , pycairo
+, pyproj
+, pytestCheckHook
+, nose
 }:
 
 let
@@ -28,18 +31,35 @@ in buildPythonPackage rec {
   };
 
   disabled = isPyPy;
-  doCheck = false; # doesn't find needed test data files
+  doCheck = true;
   preBuild = let
     pythonVersion = with stdenv.lib.versions; "${major python.version}${minor python.version}";
+    test-data = pkgs.fetchFromGitHub {
+      owner = "mapnik";
+      repo = "test-data";
+      rev = "99da07d5e76ccf5978ef0a380bf5f631f9088584";
+      sha256 = "0y0ifjw848ylfbi84yxkgxqlz0vbarggphvkz24adzzsfgxpcd58";
+    };
   in ''
     export BOOST_PYTHON_LIB="boost_python${pythonVersion}"
     export BOOST_THREAD_LIB="boost_thread"
     export BOOST_SYSTEM_LIB="boost_system"
+    rmdir test/data*
+    ln -sf ${test-data} test/data
+    ${python.interpreter} ./setup.py build_ext --inplace
   '';
 
   nativeBuildInputs = [
     mapnik # for mapnik_config
   ];
+  postInstall = ''
+    echo "env = {'ICU_DATA': '${pkgs.icu}','GDAL_DATA': '${pkgs.gdal}','PROJ_LIB': '${pkgs.proj}'}" > $out/lib/python3.8/site-packages/mapnik/mapnik_settings.py
+    echo "SEMPF1"
+    find .
+    echo "SEMPF2"
+    find $out
+
+  '';
 
   buildInputs = [
     mapnik
@@ -55,7 +75,11 @@ in buildPythonPackage rec {
     proj
     zlib
   ]);
-  propagatedBuildInputs = [ pillow pycairo ];
+  checkInputs = [
+    pytestCheckHook
+    nose
+  ];
+  propagatedBuildInputs = [ pillow pycairo pyproj ];
 
   meta = with stdenv.lib; {
     description = "Python bindings for Mapnik";
